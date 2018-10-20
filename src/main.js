94c3f1679ed1158f1f5d15d7360f744c0ff140ab
Elieen-jiang <993374385@qq.com>
2018-10-20 18:43:57 +0800

    首页静态页面基本完成

--- src/main.js
+++ src/main.js
@@ -1,16 +1,36 @@
 import Vue from 'vue'
 
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+// 导入 vue-resource   vue-resource是一个通过XMLHttpRequrest或JSONP技术实现异步加载服务端数据的Vue插件
+
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
 import app from './App.vue'
 
-import { Header } from 'mint-ui'
+// 按需导入 mint-ui 组件
+import { Header, Swipe, SwipeItem } from 'mint-ui'
 
 import './lib/mui/css/mui.css'  
 
+import './lib/mui/css/icons-extra.css'
+
 Vue.component(Header.name, Header)
+
+// 按需导入需要的轮播图的组件
+
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+// 导入自己的 router
+import router from './router.js'
 
 var vm = new Vue ({
   el: '#app',
   render: c => c(app), 
-  data: {},
-  methods: {}
+  router
 })
---
+++ src/router.js
@@ -0,0 +1,21 @@
+import VueRouter from 'vue-router'
+
+// 导入对应的路由
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
+
+var router = new VueRouter ({
+    routes: [
+        { path: '/', redirect: '/home'},
+        { path: '/home', component: HomeContainer},
+        { path: '/member', component: MemberContainer},
+        { path: '/shopcar', component: ShopcarContainer},
+        { path: '/search', component: SearchContainer},
+
+    ],
+    linkActiveClass: 'mui-active'
+})
+
+export default router
